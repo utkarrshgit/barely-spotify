0b2d372a228dca4e69440f8bc62176f2c0bd09b4
utkarrshgit <utkarshjaiswal142@gmail.com>
2026-03-23 23:03:25 +0530

    changed server port

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,10 @@
-require('dotenv').config()
-const app = require('./src/app')
-const connectDB = require('./src/db/db')
+require("dotenv").config();
+const app = require("./src/app");
+const connectDB = require("./src/db/db");
 
-connectDB()
+connectDB();
 
-app.listen(3000, () => {
-    console.log(`Server is running on port 3000`)
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
